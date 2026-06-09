c0a3694b918abb083c53a8f68ff3a2b94d6c1646
abalbin2 <abalbin2@gmail.com>
2026-06-09 16:24:34 +0200

    feat: replace Doors FAB icon with Lumi cone asset

--- components/nav/Fab.tsx
+++ components/nav/Fab.tsx
@@ -3,6 +3,7 @@
 import { useApp, CHAT_CONTEXTS } from "@/lib/store";
 
 const IMG_LUMI_ORB = "/lumi-torus.png";
+const IMG_LUMI_CONE = "/lumi-cone.png";
 
 export function Fab() {
   const screen = useApp((s) => s.screen);
@@ -44,10 +45,8 @@ export function Fab() {
             <>
               <span className="h-[56px] w-px shrink-0 bg-[#eee]" />
               <button className="flex h-[56px] shrink-0 items-center gap-1 px-6 text-[16px] font-semibold tracking-[-0.2px] text-[#191919]">
-                <svg className="h-8 w-8 shrink-0" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="6" y="4" width="20" height="26" rx="2" stroke="#191919" strokeWidth="2"/>
-                  <circle cx="21" cy="17" r="1.5" fill="#191919"/>
-                </svg>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={IMG_LUMI_CONE} alt="" className="h-9 w-9 shrink-0 object-contain" />
                 Doors
               </button>
             </>
